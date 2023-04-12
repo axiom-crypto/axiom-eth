5aa9d90d12a2c59aaa63cb22f5f59aaad0c0de6b
Jonathan Wang <jonathanpwang@users.noreply.github.com>
2023-04-12 14:35:47 +0100

    fix(bin): add srs-readonly flag to `storage_proof`
    * See previous commit about separated 'production' feature from
      srs-readonly option

--- src/bin/storage_proof.rs
+++ src/bin/storage_proof.rs
@@ -22,6 +22,8 @@ struct Cli {
     create_contract: bool,
     #[arg(long = "readonly")]
     readonly: bool,
+    #[arg(long = "srs-readonly")]
+    srs_readonly: bool,
     #[arg(short, long = "config-path")]
     config_path: Option<PathBuf>,
     #[arg(short, long = "data-path")]
@@ -31,13 +33,13 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
     #[cfg(feature = "production")]
-    let production = true;
+    let srs_readonly = true;
     #[cfg(not(feature = "production"))]
-    let production = false;
+    let srs_readonly = args.srs_readonly;
 
     let scheduler = StorageScheduler::new(
         args.network,
-        production,
+        srs_readonly,
         args.readonly,
         args.config_path.unwrap_or_else(|| PathBuf::from("configs")),
         args.data_path.unwrap_or_else(|| PathBuf::from("data")),
